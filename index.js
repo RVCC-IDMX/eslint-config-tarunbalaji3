7b0f04eb5b1c1f72843b8049bc29b45657476942
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-02-02 15:47:09 -0500

    Install prettier configuration plug-in

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* whoa! Hold your horses! This is nicely formatted code. */
 
-const message = "Hello World";
+const message = 'Hello World';
 
 console.log(message); // this should not show as an error
